1f788d21f72a9c564567e4e527b1d9782e41bf35
liveforone <yc4852@gmail.com>
2026-02-06 18:00:18 +0900

    refactor : change Request and Response dto type to api spec's Request and Response.

--- src/__test__/post.service.test.ts
+++ src/__test__/post.service.test.ts
@@ -1,8 +1,8 @@
+import { Signup } from "../api/auth/signup.api.js";
+import { CreatePost } from "../api/post/create-post.api.js";
+import { UpdatePost } from "../api/post/update-post.api.js";
 import { prisma } from "../config/prisma.js";
 import { redis } from "../config/redis.js";
-import { SignupDto } from "../dto/auth/signup.dto.js";
-import { CreatePostDto } from "../dto/post/createPost.dto.js";
-import { UpdatePostDto } from "../dto/post/updatePost.dto.js";
 import { CustomError } from "../errors/custom.error.js";
 import { AuthService } from "../services/auth.service.js";
 import { PostService } from "../services/post.service.js";
@@ -25,12 +25,12 @@ describe("PostService Unit Test(Real DB / Redis)", () => {
   it("Create Post Test [Success]", async () => {
     const username = "create_post_test";
     const password = "create_post_test_password";
-    const signupDto: SignupDto = { username, password };
+    const signupDto: Signup.Request = { username, password };
     const user = await AuthService.signup(signupDto);
 
     const title = "create_post_title";
     const content = "test_content";
-    const createPostDto: CreatePostDto = { title, content };
+    const createPostDto: CreatePost.Request = { title, content };
     const postId = await PostService.createPost(createPostDto, user.id);
 
     const post = await PostService.getPostById(postId);
@@ -40,33 +40,33 @@ describe("PostService Unit Test(Real DB / Redis)", () => {
   it("Create Post Test [Fail - Wrong id]", async () => {
     const username = "create_post_fail_test";
     const password = "create_post_fail_test_password";
-    const signupDto: SignupDto = { username, password };
+    const signupDto: Signup.Request = { username, password };
     await AuthService.signup(signupDto);
 
     const title = "create_post_fail_title";
     const content = "test_content";
-    const createPostDto: CreatePostDto = { title, content };
+    const createPostDto: CreatePost.Request = { title, content };
     await expect(
-      PostService.createPost(createPostDto, "worng_userId")
+      PostService.createPost(createPostDto, "worng_userId"),
     ).rejects.toThrow(
-      "[P2003] The foreign key constraint has failed because the associated record does not exist in the referenced table. This indicates that the value being inserted or updated in the column with the foreign key constraint does not have a corresponding entry in the related table. Check the integrity of your data and ensure that all foreign key references are valid."
+      "[P2003] The foreign key constraint has failed because the associated record does not exist in the referenced table. This indicates that the value being inserted or updated in the column with the foreign key constraint does not have a corresponding entry in the related table. Check the integrity of your data and ensure that all foreign key references are valid.",
     );
   });
 
   it("Update Post Test [Success]", async () => {
     const username = "update_post_test";
     const password = "update_post_test_password";
-    const signupDto: SignupDto = { username, password };
+    const signupDto: Signup.Request = { username, password };
     const user = await AuthService.signup(signupDto);
 
     const title = "update_post_title";
     const content = "test_content";
-    const createPostDto: CreatePostDto = { title, content };
+    const createPostDto: CreatePost.Request = { title, content };
     const postId = await PostService.createPost(createPostDto, user.id);
 
     const updatedTitle = "updated_title";
     const updatedContent = "updated_content";
-    const updatePostDto: UpdatePostDto = {
+    const updatePostDto: UpdatePost.Request = {
       title: updatedTitle,
       content: updatedContent,
     };
@@ -79,17 +79,17 @@ describe("PostService Unit Test(Real DB / Redis)", () => {
   it("Remove Post Test [Success]", async () => {
     const username = "remove_post_test";
     const password = "remove_post_test_password";
-    const signupDto: SignupDto = { username, password };
+    const signupDto: Signup.Request = { username, password };
     const user = await AuthService.signup(signupDto);
 
     const title = "remove_post_title";
     const content = "test_content";
-    const createPostDto: CreatePostDto = { title, content };
+    const createPostDto: CreatePost.Request = { title, content };
     const postId = await PostService.createPost(createPostDto, user.id);
 
     await PostService.removePost(postId, user.id);
     await expect(PostService.getPostById(postId)).rejects.toBeInstanceOf(
-      CustomError
+      CustomError,
     );
   });
 });
