a08bd0202693875f9ffdea45c7f9f05a49392817
liveforone <yc4852@gmail.com>
2026-02-06 17:58:04 +0900

    refactor : change Request and Response dto type to api spec's Request and Response.

--- src/__test__/auth.service.test.ts
+++ src/__test__/auth.service.test.ts
@@ -2,9 +2,9 @@ import { AuthService } from "../services/auth.service.js";
 import bcrypt from "bcrypt";
 import { prisma } from "../config/prisma.js";
 import { redis } from "../config/redis.js";
-import { SignupDto } from "../dto/auth/signup.dto.js";
-import { LoginDto } from "../dto/auth/login.dto.js";
-import { UpdatePasswordDto } from "../dto/auth/updatePassword.dto.js";
+import { Signup } from "../api/auth/signup.api.js";
+import { Login } from "../api/auth/login.api.js";
+import { UpdatePassword } from "../api/auth/update-password.api.js";
 
 describe("AuthService Unit Test(Real DB / Redis)", () => {
   beforeEach(async () => {
@@ -22,7 +22,10 @@ describe("AuthService Unit Test(Real DB / Redis)", () => {
   it("Signup Test [Success]", async () => {
     const username = "signup_test";
     const password = "test";
-    const signupDto: SignupDto = { username: username, password: password };
+    const signupDto: Signup.Request = {
+      username: username,
+      password: password,
+    };
 
     const user = await AuthService.signup(signupDto);
 
@@ -34,10 +37,13 @@ describe("AuthService Unit Test(Real DB / Redis)", () => {
   it("Login Test [Success]", async () => {
     const username = "login_test";
     const password = "login_test_password";
-    const signupDto: SignupDto = { username: username, password: password };
+    const signupDto: Signup.Request = {
+      username: username,
+      password: password,
+    };
     await AuthService.signup(signupDto);
 
-    const loginDto: LoginDto = { username: username, password: password };
+    const loginDto: Login.Request = { username: username, password: password };
     const user = await AuthService.login(loginDto);
 
     expect(user.username).toBe(username);
@@ -46,28 +52,40 @@ describe("AuthService Unit Test(Real DB / Redis)", () => {
   it("Login Test [Fail - Wrong Password]", async () => {
     const username = "login_fail_test";
     const password = "login_fail_test_password";
-    const signupDto: SignupDto = { username: username, password: password };
+    const signupDto: Signup.Request = {
+      username: username,
+      password: password,
+    };
     await AuthService.signup(signupDto);
 
     const wrongPassword = "wrong_password";
-    const loginDto: LoginDto = { username: username, password: wrongPassword };
+    const loginDto: Login.Request = {
+      username: username,
+      password: wrongPassword,
+    };
     await expect(AuthService.login(loginDto)).rejects.toThrow("Wrong Password");
   });
 
   it("UpdatePassword Test [Success]", async () => {
     const username = "update_password_test";
     const password = "update_password_test_password";
-    const signupDto: SignupDto = { username: username, password: password };
+    const signupDto: Signup.Request = {
+      username: username,
+      password: password,
+    };
     const user = await AuthService.signup(signupDto);
 
     const newPassword = "new_password";
-    const updatePasswordDto: UpdatePasswordDto = {
+    const updatePasswordDto: UpdatePassword.Request = {
       originalPassword: password,
       newPassword,
     };
     await AuthService.updatePassword(updatePasswordDto, user.id);
 
-    const loginDto: LoginDto = { username: username, password: newPassword };
+    const loginDto: Login.Request = {
+      username: username,
+      password: newPassword,
+    };
     const foundUser = await AuthService.login(loginDto);
     expect(foundUser.username).toBe(username);
   });
@@ -75,17 +93,20 @@ describe("AuthService Unit Test(Real DB / Redis)", () => {
   it("UpdatePassword Test [Fail - Wrong Password]", async () => {
     const username = "update_password_fail_test";
     const password = "update_password_fail_test_password";
-    const signupDto: SignupDto = { username: username, password: password };
+    const signupDto: Signup.Request = {
+      username: username,
+      password: password,
+    };
     const user = await AuthService.signup(signupDto);
 
     const wrongPassword = "wrong_password";
     const newPassword = "new_password";
-    const updatePasswordDto: UpdatePasswordDto = {
+    const updatePasswordDto: UpdatePassword.Request = {
       originalPassword: wrongPassword,
       newPassword: newPassword,
     };
     await expect(
-      AuthService.updatePassword(updatePasswordDto, user.id)
+      AuthService.updatePassword(updatePasswordDto, user.id),
     ).rejects.toThrow("Wrong Password");
   });
 });
