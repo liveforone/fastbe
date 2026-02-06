f4c92c2e957de805d66c60851f0b8a4dbd6771fb
liveforone <yc4852@gmail.com>
2026-02-06 18:00:11 +0900

    refactor : request dto and response dto type change to api spec Request and Response.

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -1,27 +1,29 @@
+import { CreatePost } from "../api/post/create-post.api.js";
+import { PostBelongWriter } from "../api/post/post-belong-writer.api.js";
+import { PostDetail } from "../api/post/post-detail.api.js";
+import { PostHome } from "../api/post/post-home.api.js";
+import { PostSearch } from "../api/post/post-search.api.js";
+import { UpdatePost } from "../api/post/update-post.api.js";
 import { prisma } from "../config/prisma.js";
-import { CreatePostDto } from "../dto/post/createPost.dto.js";
-import { PostInfoDto } from "../dto/post/postInfo.dto.js";
-import { PostPageDto } from "../dto/post/postPage.dto.js";
-import { UpdatePostDto } from "../dto/post/updatePost.dto.js";
 import { withPrismaError } from "../errors/prismaError.handle.js";
 
 export class PostService {
   static async createPost(
-    createPostDto: CreatePostDto,
-    userId: string
+    createPostDto: CreatePost.Request,
+    userId: string,
   ): Promise<bigint> {
     const { title, content } = createPostDto;
     const post = await withPrismaError(() =>
-      prisma.post.create({ data: { title, content, writer_id: userId } })
+      prisma.post.create({ data: { title, content, writer_id: userId } }),
     );
 
     return post.id;
   }
 
   static async updatePost(
-    updatePostDto: UpdatePostDto,
+    updatePostDto: UpdatePost.Request,
     id: bigint,
-    userId: string
+    userId: string,
   ) {
     const { title, content } = updatePostDto;
     await withPrismaError(() =>
@@ -35,7 +37,7 @@ export class PostService {
           content: content,
           post_state: "EDITED",
         },
-      })
+      }),
     );
   }
 
@@ -46,15 +48,15 @@ export class PostService {
           id: id,
           writer_id: userId,
         },
-      })
+      }),
     );
   }
 
-  static async getPostById(id: bigint): Promise<PostInfoDto> {
+  static async getPostById(id: bigint): Promise<PostDetail.Response> {
     return await withPrismaError(() =>
       prisma.post.findUniqueOrThrow({
         where: { id: id },
-      })
+      }),
     );
   }
 
@@ -62,7 +64,7 @@ export class PostService {
    * You have to add "setReplySerializer" setting in server.ts
    * This make bigint type work.
    */
-  static async getAllPostPages(lastId?: bigint): Promise<PostPageDto> {
+  static async getAllPostPages(lastId?: bigint): Promise<PostHome.Response> {
     const posts = await withPrismaError(() =>
       prisma.post.findMany({
         take: 10,
@@ -79,11 +81,11 @@ export class PostService {
           content: true,
           post_state: true,
         },
-      })
+      }),
     );
 
     const newLastId =
-      posts.length > 0 ? posts[posts.length - 1].id : lastId ?? 0n;
+      posts.length > 0 ? posts[posts.length - 1].id : (lastId ?? 0n);
     return {
       postSummaries: posts,
       metadata: {
@@ -94,8 +96,8 @@ export class PostService {
 
   static async getPostPagesByWriter(
     userId: string,
-    lastId?: bigint
-  ): Promise<PostPageDto> {
+    lastId?: bigint,
+  ): Promise<PostBelongWriter.Response> {
     const posts = await withPrismaError(() =>
       prisma.post.findMany({
         where: { writer_id: userId },
@@ -113,11 +115,11 @@ export class PostService {
           content: true,
           post_state: true,
         },
-      })
+      }),
     );
 
     const newLastId =
-      posts.length > 0 ? posts[posts.length - 1].id : lastId ?? 0n;
+      posts.length > 0 ? posts[posts.length - 1].id : (lastId ?? 0n);
     return {
       postSummaries: posts,
       metadata: {
@@ -128,8 +130,8 @@ export class PostService {
 
   static async searchPostPages(
     keyword: string,
-    lastId?: bigint
-  ): Promise<PostPageDto> {
+    lastId?: bigint,
+  ): Promise<PostSearch.Response> {
     const posts = await withPrismaError(() =>
       prisma.post.findMany({
         where: { title: { startsWith: keyword } },
@@ -147,11 +149,11 @@ export class PostService {
           content: true,
           post_state: true,
         },
-      })
+      }),
     );
 
     const newLastId =
-      posts.length > 0 ? posts[posts.length - 1].id : lastId ?? 0n;
+      posts.length > 0 ? posts[posts.length - 1].id : (lastId ?? 0n);
     return {
       postSummaries: posts,
       metadata: {
