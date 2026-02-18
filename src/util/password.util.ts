314999840f4482de6701d7a4518e51ddb237237b
liveforone <yc4852@gmail.com>
2026-02-18 16:01:57 +0900

    feat : Exclude password util function to outer dir.

---
+++ src/util/password.util.ts
@@ -0,0 +1,17 @@
+import bcrypt from "bcrypt";
+import { GlobalError } from "../errors/global.error.js";
+
+export async function verifyPassword(
+  plain: string,
+  hashed: string,
+): Promise<void> {
+  const valid = await bcrypt.compare(plain, hashed);
+  if (!valid) {
+    const errorMsg = "Wrong Password";
+    throw new GlobalError(errorMsg, 401);
+  }
+}
+
+export async function hashPassword(password: string): Promise<string> {
+  return await bcrypt.hash(password, 10);
+}
